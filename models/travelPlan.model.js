432eb5b67959a1731b3180d5c83dde60537c109b
lakshay bansal <blakshay93@gmail.com>
2016-06-10 13:06:12 +0530

    Rectified some error

--- models/travelPlan.model.js
+++ models/travelPlan.model.js
@@ -5,22 +5,20 @@ var Q = require('q');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var TravelPlanSchema = mongoose.Schema({
-  travelPlanId:String,
   components:[{
   type:String,
-  cityName:{type:String,required:false},
   essentials:Schema.Types.Mixed,
-  childServices:Schema.Types.Mixed,}]
+  childServices:Schema.Types.Mixed}]
 });
 
 
 TravelPlanSchema.statics.getTravelPlan=function getTravelPlan(travelPlanId) {
       var deferred = Q.defer();
-      this.findOne({travelPlanId: travelPlanId})
+      this.findOne({_id:travelPlanId})
                 .exec(function(err,data){
                   travelPlanData = data;
                    deffered.resolve(travelPlanData);
-                 })
+                 });
 
     return deferred.promise;
 }
@@ -30,7 +28,7 @@ TravelPlanSchema.statics.postTravelPlan=function postTravelPlan(travelPlandata) 
     var TravelPlandata=  this.create(travelPlandata);
       TravelPlandata.save(function(err){
       if ( err ) console.log(err);
-      console.log(TravelPlandata.travelPlanId +" Saved Successfully");
+      deferred.resolve(TravelPlandata._id);
     });
 
 
@@ -39,21 +37,19 @@ TravelPlanSchema.statics.postTravelPlan=function postTravelPlan(travelPlandata) 
 
 TravelPlanSchema.statics.putTravelPlan=function putTravelPlan(id,travelPlanNew) {
     var deferred = Q.defer();
-     var TravelPlandata=this.create(
-      { travelPlanId:id,
-       components:travelPlanNew
-     });
-     TravelPlandata.save(function(err){
+
+     this.findByIdAndUpdate(id,travelPlanNew,{new:true},function(err,data){
        if ( err ) console.log(err);
-       console.log(TravelPlandata.travelPlanId +" Updated Successfully");
+       deferred.resolve(data._id);
      });
     return deferred.promise;
 }
 
 TravelPlanSchema.statics.deleteTravelPlan=function deleteTravelPlan(id) {
     var deferred = Q.defer();
-    this.remove({ travelPlanId: id }, function (err) {
+    this.findByIdAndRemove(id, function (err,data) {
         if(err)  console.log(err);
+        deferred.resolve(data);
  })
     return deferred.promise;
 }
